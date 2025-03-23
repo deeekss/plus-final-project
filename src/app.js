0869c2ec3af976267f2444ed54e9bcec869fb8df
deeekss <eni.diko@gmail.com>
2025-03-23 19:14:56 +0000

    added class for the forecast icons in JS and updated styling

--- src/app.js
+++ src/app.js
@@ -80,7 +80,7 @@ function displayForecast(response) {
   <div class="weather-forecast-day">
     <div class="weather-forecast-date">Sun</div>
     <div class="weather-forecast-icon">
-     <img src="${day.condition.icon_url}"/>
+     <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
     </div>
     <div class="weather-forecast-temperatures">
       <div class="weather-forecast-temperature">
